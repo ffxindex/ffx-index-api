9edc9a366b1c3136891ba984b0ec714c7a857af4
Kieran Eglin <kieran.eglin@gmail.com>
2016-05-10 13:21:50 -0700

    Added request logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,10 +24,12 @@ class ApplicationController < ActionController::API
   end
 
   def validate_api_key
+    # NOTE: This only gets hit if asset is not served by rack::cache.
+    # This is fine for my application, however you may want to fix this.
     api_user = ApiUser.find_by(api_key: get_api_key)
 
     if api_user.present? && api_user.permitted
-
+      api_user.api_requests.create(ip: request.remote_ip)
     else
       @code = 401
       @data = 'unauthorized - please check your API key'
